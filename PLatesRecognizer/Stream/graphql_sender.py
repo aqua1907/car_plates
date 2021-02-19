f191b8891b77f035c07768caa43edcca2c7c69da
vanou <vanoufo@gmail.com>
2021-02-19 18:34:47 +0200

    Clean code. Add logic to not sending every time last record of results.

--- PLatesRecognizer/Stream/graphql_sender.py
+++ PLatesRecognizer/Stream/graphql_sender.py
@@ -1,8 +1,9 @@
 import graphene
 import json
+import os
 from graphene.types.generic import GenericScalar
 from configobj import ConfigObj
-import os
+import dateutil.parser
 
 
 class CarInfo(graphene.ObjectType):
@@ -20,7 +21,7 @@ class Query(graphene.ObjectType):
     """
         The class that represents schema for query execution
     """
-    carInfo = graphene.Field(CarInfo, results=GenericScalar())
+    carInfo = graphene.Field(CarInfo, result=GenericScalar())
     camera = graphene.String(camera_id=graphene.String())  # add info about from which camera results were captured
 
     def resolve_carInfo(self, info, result):
@@ -48,6 +49,20 @@ class Query(graphene.ObjectType):
         return camera_id
 
 
+def read_jsonl(path):
+    """
+    Read last record in JSONL file. Last record is a first line in the file.
+    :param path: String. Path to the file
+    :return: Dict
+    """
+    with open(filename) as file_obj:  # read file
+        line = next(iter(file_obj))  # get first line of file with results
+        data = json.loads(line)  # load data
+    file_obj.close()
+
+    return data
+
+
 if __name__ == "__main__":
     config = ConfigObj(r"config.ini")  # read Platerecognizer config file
     filename = config['cameras']['camera-1']['jsonlines_file']  # get filename of results
@@ -69,14 +84,17 @@ if __name__ == "__main__":
                     }
                 }
             """
+    data = read_jsonl(filename)
+    last_timestamp = dateutil.parser.isoparse(data["timestamp"])    # get_last record in results
     while True:
         if os.path.exists(filename):  # check file with results exists
             if os.path.getsize(filename) > 0:  # check if file is not empty
-                with open(filename) as file_obj1:  # read file
-                    line = next(iter(file_obj1))  # get first line of file with results
-                    data = json.loads(line)  # load data
+                data = read_jsonl(filename)
+                new_timestamp = dateutil.parser.isoparse(data["timestamp"])
+                t_delta = (new_timestamp - last_timestamp).total_seconds()
+                if t_delta > 0:
                     for result in data["results"]:
                         # Loop for each detect plate and execute query with variables
                         out = schema.execute(query, variables={"cameraId": data["camera_id"],
                                                                "result": result})
-                        print(json.dumps(out.data, indent=4))
+                        print(out)
