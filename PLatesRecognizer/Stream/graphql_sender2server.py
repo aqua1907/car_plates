12798f5889822d912aa3b5d11f2e45eafe597d8c
vanou <vanoufo@gmail.com>
2021-03-02 18:41:09 +0200

    Change logic

--- PLatesRecognizer/Stream/graphql_sender2server.py
+++ PLatesRecognizer/Stream/graphql_sender2server.py
@@ -1,24 +1,32 @@
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
-import json
 from configobj import ConfigObj
-import dateutil.parser
+import json
 import os
-import datetime
 
 
 def read_jsonl(path):
     """
-    Read last record in JSONL file. Last record is a first line in the file.
+    Read last record in JSONL file. Last record is a last line in the file.
     :param path: String. Path to the file
     :return: Dict
     """
     with open(path) as file_obj:  # read file
-        line = next(iter(file_obj))  # get first line of file with results
+        line = file_obj.read().splitlines()[-1]  # get last line of file with results
         data = json.loads(line)  # load data
     file_obj.close()
 
     return data
+
+
+def compute_area(bbox):
+    x1, y1 = bbox["xmin"], bbox["ymin"]
+    x2, y2 = bbox["xmax"], bbox["ymax"]
+
+    h, w = x2 - x1, y2 - y1
+    area = h * w
+
+    return area
 
 
 def main():
@@ -38,43 +46,78 @@ def main():
     client = Client(transport=transport, fetch_schema_from_transport=True)
 
     config = ConfigObj(r"config.ini")  # read Platerecognizer config file
-    filename = config['cameras']['camera-1']['jsonlines_file']  # get filename of results
+    filename = config['cameras']['camera-2']['jsonlines_file']  # get filename of results
 
     if os.path.exists(filename):  # check file with results exists
         if os.path.getsize(filename) > 0:  # check if file is not empty
             data = read_jsonl(filename)
-            last_timestamp = dateutil.parser.isoparse(data["timestamp_local"])
-            # print(last_timestamp)
+            last_filesize = os.path.getsize(filename)
+            last_license_plate = data['results'][0]['plate']
         else:
-            last_timestamp = datetime.datetime.now().astimezone().isoformat()
-            last_timestamp = dateutil.parser.isoparse(last_timestamp)  # Convert str time iso format to datetime
+            last_filesize = 0
+            last_license_plate = ''
     else:
-        # if file is does not exist time equals datetime.now()
-        last_timestamp = datetime.datetime.now().astimezone().isoformat()
-        last_timestamp = dateutil.parser.isoparse(last_timestamp)  # Convert str time iso format to datetime
+        last_filesize = 0
+        last_license_plate = ''
 
-    params = ""
+    send = False
+    counter_front = 0
+    counter_back = 0
+
     while True:
         if os.path.exists(filename):  # check file with results exists
             if os.path.getsize(filename) > 0:  # check if file is not empty
-                data = read_jsonl(filename)
-                new_timestamp = dateutil.parser.isoparse(data["timestamp_local"])
-                t_delta = (new_timestamp - last_timestamp).total_seconds()
-                if t_delta > 0:
-                    last_timestamp = new_timestamp
-                    # rename camera- to gate while keeping camera_id
-                    data["camera_id"] = data["camera_id"].replace("camera-", "gate")
-                    for result in data["results"]:
+                new_filesize = os.path.getsize(filename)
+                if new_filesize > last_filesize:
+                    last_filesize = new_filesize
+                    data = read_jsonl(filename)
+                    print(f"Counter front = {counter_front}")
+                    print(f"Counter back = {counter_back}")
+
+                    if data['results'][0]['vehicle']["score"] != 0.0:
+                        new_license_plate = data['results'][0]['plate']
+                        print(new_license_plate)
+                        if new_license_plate != last_license_plate:
+                            last_license_plate = new_license_plate
+                            initial_bbox = data['results'][0]['box']
+                            initial_box_area = compute_area(initial_bbox)
+                            print(f"Initial bbox = {initial_box_area}")
+                        else:
+                            bbox = data['results'][0]['box']
+                            box_area = compute_area(bbox)
+                            print(f"bbox = {box_area}")
+
+                            if box_area > initial_box_area:
+                                counter_front += 1
+                                if counter_front == 7:
+                                    is_front = "true"
+                                    send = True
+                            elif box_area < initial_box_area:
+                                counter_back += 1
+                                if counter_back == 7:
+                                    is_front = "false"
+                                    send = True
+
+                elif new_filesize == last_filesize:
+                    if send:
+                        data = read_jsonl(filename)
+                        # rename camera- to gate while keeping camera_id
+                        data["camera_id"] = data["camera_id"].replace("camera-", "gate")
+                        result = data["results"][0]
+
                         # Create queryString and pass as variable
                         params = '{"carColor": "%s", "carMark": "%s", "carNumber": "%s", ' \
                                  '"gateId": "%s", "id": "%s", "isFront": %s, ' \
                                  '"parkingId": "%s", "time": "%s"}' % ("", "", result["plate"],
-                                                                       data["camera_id"], "", "true",
-                                                                       "", last_timestamp)
+                                                                       data["camera_id"], "", is_front,
+                                                                       "", data["timestamp_local"])
 
-                    query = gql(query_code)
-                    result = client.execute(query, variable_values={"params": params})  # Execute query
-                    print(result)
+                        query = gql(query_code)
+                        result = client.execute(query, variable_values={"params": params})  # Execute query
+                        print(result)
+                        send = False
+                        counter_front = 0
+                        counter_back = 0
 
 
 if __name__ == "__main__":
